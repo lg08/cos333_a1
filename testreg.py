d9d3e6a0af0bf775a94f2aee18194a825502acbb
Lucas Gen <lg08@pop-os.localdomain>
2021-02-21 16:13:12 -0500

    more testing

--- testreg.py
+++ testreg.py
@@ -12,21 +12,24 @@ courseid = cursor.fetchall()
 cursor.close()
 connection.close()
 
+# checks the different departments
 for count, area in enumerate(courseid):
-    ourProg = 'python reg.py -d "{}"'.format(area[0])
-    refProg = 'python ref_reg.pyc -d "{}"'.format(area[0])
-    system(ourProg + ' > ourFile')
-    system(refProg + ' > refFile')
-    system('diff ourFile refFile >> result')
-    system('rm -f ourFile refFile')
+    for item in ['-d', '-t', '-n', '-a']
+        ourProg = 'python reg.py {} "{}"'.format(item, area[0])
+        refProg = 'python ref_reg.pyc {} "{}"'.format(item, area[0])
+        system(ourProg + ' > ourFile')
+        system(refProg + ' > refFile')
+        system('diff ourFile refFile >> result')
+        system('rm -f ourFile refFile')
     if (count == 100):
         break
 
+# checks wildcard shit
 
-#for count,id in enumerate(courseid):
-#    ourProg = 'python regdetails.py {}'.format(id[0])
-#    refProg = 'python ref_regdetails.pyc {}'.format(id[0])
-#    system(ourProg + ' > ourFile')
-#    system(refProg + ' > refFile')
-#    system('diff ourFile refFile >> result')
-#    system('rm -f ourFile refFile')
+for count,id in enumerate(courseid):
+   ourProg = 'python regdetails.py {}'.format(id[0])
+   refProg = 'python ref_regdetails.pyc {}'.format(id[0])
+   system(ourProg + ' > ourFile')
+   system(refProg + ' > refFile')
+   system('diff ourFile refFile >> result')
+   system('rm -f ourFile refFile')
